d9bf24f574a654947e9ab7ad365ea98ba66eb5b2
Aaron <java.jaydev@gmail.com>
2023-04-17 18:12:11 +0900

    #스프링부트 강좌 72강(블로그 프로젝트) - 회원가입 문제와 게시글 삭제 문제 해결

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -28,9 +28,12 @@ let index = {
 			contentType: "application/json; charset=utf-8", // body 데이터가 어떤 타입인지 (MIME)
 			dataType: "json",  // 응답이 왔을때 javascript object 로 변경해줌
 		}).done(function(resp) {
-			alert("회원가입이 완료되었습니다.");
-			// alert(JSON.stringify(resp));
-			location.href="/";
+			if (resp.status === 500) {
+				alert("회원가입에 실패하였습니다.");
+			} else {
+				alert("회원가입이 완료되었습니다.");
+				location.href="/";
+			}
 		}).fail(function(error) {
 			alert(JSON.stringify(error));
 		});
